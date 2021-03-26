7bf8636908c256650c0749b82991a4f1bc9bdb16
Matthew Lee <sangminleeus@gmail.com>
2021-03-27 07:28:17 +0900

    Update search feature

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,6 +1,25 @@
 export const homeController = (req, res) => res.render("home", {pageTitle: "Home"});
-export const searchController = (req,res) => res.render("search", {pageTitle: "Search"});
-export const uploadController = (req,res) => res.render("upload", {pageTitle: "Upload"});
-export const videoDetailController = (req,res) => res.render("videoDetail" , {pageTiel: "Video Detail"});
-export const editVideoController = (req,res) => res.render("editVideo" , {pageTiel: "Edit Video"});
-export const deleteVideoController = (req,res) => res.render("deleteVideo" , {pageTiel: " Delete Video"});
+
+export const searchController = (req,res) => {
+    // const searchingBy = req.query.term
+    const {
+        query: {term: searchingBy}
+    } = req;
+    res.render("search", {pageTitle: "Search", searchingBy})
+};
+
+export const uploadController = (req,res) => {
+    res.render("upload", {pageTitle: "Upload"})
+};
+
+export const videoDetailController = (req,res) => {
+    res.render("videoDetail" , {pageTiel: "Video Detail"})
+};
+
+export const editVideoController = (req,res) => {
+    res.render("editVideo" , {pageTiel: "Edit Video"})
+};
+
+export const deleteVideoController = (req,res) => {
+    res.render("deleteVideo" , {pageTiel: " Delete Video"})
+};
