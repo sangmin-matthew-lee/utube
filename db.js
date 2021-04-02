8a86d6d56c68196ad4eca1444548c0abf514c646
Matthew Lee <sangminleeus@gmail.com>
2021-04-02 18:21:56 +0900

    update logs

--- db.js
+++ db.js
@@ -9,11 +9,11 @@ mongoose.connect("mongodb://localhost:27017/u-tube", {
 const db = mongoose.connection;
 
 const handleOpen = () => {
-    console.log("Connected to DB");
+    console.log("✅ Connected to DB");
 }
 
 const handleError = (error) => {
-    console.log("Error on DB Connection");
+    console.log(`❌ Error on DB Connection: ${error}`);
 }
 db.once("open", handleOpen);
 db.on("error", handleError);
--- init.js
+++ init.js
@@ -3,6 +3,6 @@ import app from "./app.js";
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`Listening on: Http://localhost:${PORT}`);
+const handleListening = () => console.log(`✅ Listening on: Http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
