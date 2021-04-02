66993e989b2b155abf5f809dd020b1b98e4b7ab5
Matthew Lee <sangminleeus@gmail.com>
2021-04-02 17:51:37 +0900

    fix bugs

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb://localhost:27017/u-tube", {
-    userNewUrlParser: true,
+    useUnifiedTopology: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 });
