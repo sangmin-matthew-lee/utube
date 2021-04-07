a0b157fa092989985abe03fb826b033c2775cf73
Sangmin Lee <sangminleeus@gmail.com>
2021-04-08 02:03:24 +0900

    Update delete and edit features

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import routes from "../routes.js";
 import { deleteVideoController, 
-    editVideoController, 
+    getEditVideoController, 
     getUploadController,
+    postEditVideoController,
     postUploadController,
     videoDetailController, 
 } from '../controller/videoController.js';
@@ -11,11 +12,19 @@ import { uploadVideo } from '../middleWares.js';
 const videoRouter = express.Router();
 
 //videoRouter.get(routes.videos, videosController)
+
+//upload
 videoRouter.get(routes.upload, getUploadController);
 videoRouter.post(routes.upload, uploadVideo, postUploadController);
 
+//video details
 videoRouter.get(routes.videoDetail(), videoDetailController);
-videoRouter.get(routes.editVideo, editVideoController);
-videoRouter.get(routes.deleteVideo, deleteVideoController);
+
+//edit video
+videoRouter.get(routes.editVideo(), getEditVideoController);
+videoRouter.post(routes.editVideo(), postEditVideoController);
+
+//delete video
+videoRouter.get(routes.deleteVideo(), deleteVideoController);
 
 export default videoRouter;
